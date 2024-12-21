74aec6c81ee2dd8d9d0970a23e66393f6aac00e0
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 13:22:28 +0200

    Added the Hero-Section to the Landing page

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+      {/* Add other sections as needed */}
+    </div>
+  );
+};
+
+export default LandingPage;
+
+
