623725918ec6896f11929dd8e9a4eeb70f0bacbb
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 23:43:41 +0200

    Added a Logo image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/SignUp" element={<SignUpPage />} />
     </Routes>
   );
 };
