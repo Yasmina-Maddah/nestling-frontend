2d34abd7526cec5b671244a5262e313e59258083
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 20:43:10 +0200

    Edited the Landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,6 +4,8 @@ import HeroSection from '../../components/HeroSection/HeroSection';
 import Features from '../../components/Features/Features';
 import HowItWorks from '../../components/HowItWorks/HowItWorks';
 import Resources from '../../components/Resources/Resources';
+import Resources from '../../components/AboutUs/AboutUs';
+
 
 
 const LandingPage = () => {
