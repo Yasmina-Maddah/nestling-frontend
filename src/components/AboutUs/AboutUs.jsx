4ad22cf69fe817a543ab9cb07e15f31919e2e2bc
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 21:02:22 +0200

    Edited the style of both How-It-Works section, and About-Us section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -9,9 +9,7 @@ const AboutUs = () => {
         <div className="about-us-text">
           <h2 className="about-us-title">About Us</h2>
           <p className="about-us-description">
-            We are a platform dedicated to empowering individuals and families by providing
-            tools, resources, and experiences that foster growth and learning. Our mission is
-            to make a positive impact in every home we touch.
+          At Nestling, we empower parents and caregivers to support their children’s growth through engaging and personalized tools. Using AI technology, we offer solutions like story visualizations, skill recommendations, and progress tracking to make learning fun and effective. With Nestling, you can nurture creativity, focus on key skills, and celebrate milestones while accessing curated resources to enhance your child’s journey. Together, we aim to unlock your child’s full potential and build a brighter future.
           </p>
         </div>
         <div className="about-us-image-container">
