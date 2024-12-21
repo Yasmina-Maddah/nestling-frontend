fb1e3862843c65a858d7b4455596286225020e42
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 13:19:55 +0200

    Wrote the Hero-Section.jsx code

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-overlay">
+        <h1 className="hero-title">Nestling</h1>
+        <p className="hero-subtitle">Empowering Growth, One Story at a Time!</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
+
