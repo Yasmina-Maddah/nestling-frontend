1b020fe23a475e10c389049361c63e4fdefad24c
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 14:33:32 +0200

    Added the navbar to the landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 import HeroSection from '../../components/HeroSection/HeroSection';
 
 const LandingPage = () => {
   return (
     <div>
+      <Navbar />
       <HeroSection />
       {/* Add other sections as needed */}
     </div>
