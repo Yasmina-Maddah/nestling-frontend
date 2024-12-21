64bba3207668d93c00236096a11c8682ad41a8e4
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 14:34:16 +0200

    Wrote the code structure of the navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import Logo from '../../assets/logo/1.png';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={Logo} alt="Nestling Logo" />
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#features">Features</a></li>
+        <li><a href="#how-it-works">How It Works</a></li>
+        <li><a href="#resources">Resources</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="navbar-buttons">
+        <button className="navbar-button login-button">Login</button>
+        <button className="navbar-button signup-button">Sign Up</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
