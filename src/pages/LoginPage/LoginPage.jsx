c6c9c13eaee5e5dbb5ab8ca7b991baf3970f9c2c
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 23:53:28 +0200

    Wrote the structure code of the Login page

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LoginPage.css';
+import Logo from '../../assets/logo/2.png';
+
+const Login = () => {
+    return (
+        <div className="container">
+            <img src={Logo} alt="Nestling Logo" className="logo" />
+            <h1>Nestling</h1>
+            <p>"Unlocking Potential, One Story at a Time"</p>
+            <form>
+                <input type="email" placeholder="Email" required className="input-field" />
+                <input type="password" placeholder="Password" required className="input-field" />
+                <button type="submit" className="submit-button">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
