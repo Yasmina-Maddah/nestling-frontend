cee72f256c99a68b77032f7987d886a68fda2d80
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-21 21:32:38 +0200

    Edited the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './Footer.css';
 import Icon from "../../assets/icons/InstagramIcon.png";
-import Logo from "../../assets/logo/1.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <img
-          src={Logo} 
-          alt="Logo"
-          className="footer-logo"
-        />
         <p className="footer-text">
           Stay updated by following us on Instagram
         </p>
