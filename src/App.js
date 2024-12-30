c12796dfba7767aa8f16820737c57f94732c4597
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-30 15:26:07 +0200

    Added more icons and images to the assets folde4r

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
-import SignUpPage from './pages/SignUpPage/SignUpPage';
-import LoginPage from './pages/LoginPage/LoginPage';
+
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/SignUp" element={<SignUpPage />} />
-      <Route path="/Login" element={<LoginPage />} />
+      
     </Routes>
   );
 };
