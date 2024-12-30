9b94b78354424dfa53f76493a531145ad6714d03
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-30 15:26:52 +0200

    Coded the Sidebar as a reusable component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Sidebar.css';
+
+// Import images from the src/assets folder
+import logo from '../../assets/logo/1.png';
+import homeIcon from '../../assets/icons/Home.png';
+import userIcon from '../../assets/icons/Profile.png';
+import chartIcon from '../../assets/icons/Chart.png';
+import bookIcon from '../../assets/icons/Book.png';
+import resourceIcon from '../../assets/icons/Resource.png';
+import arrowRightIcon from '../../assets/icons/Arrow.png';
+
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-logo">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <nav>
+        <ul className="nav-list">
+          <li>
+            <button className="nav-button">
+              <img src={homeIcon} alt="Home" className="nav-icon" />
+            </button>
+          </li>
+          <li>
+            <button className="nav-button">
+              <img src={userIcon} alt="Profile" className="nav-icon" />
+            </button>
+          </li>
+          <li>
+            <button className="nav-button">
+              <img src={chartIcon} alt="Analytics" className="nav-icon" />
+            </button>
+          </li>
+          <li>
+            <button className="nav-button">
+              <img src={bookIcon} alt="Library" className="nav-icon" />
+            </button>
+          </li>
+          <li>
+            <button className="nav-button">
+              <img src={resourceIcon} alt="Ideas" className="nav-icon" />
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <div className="sidebar-footer">
+        <button className="nav-button">
+          <img src={arrowRightIcon} alt="Next" className="nav-icon" />
+        </button>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
