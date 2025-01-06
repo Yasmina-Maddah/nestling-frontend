498132e9fcbdcffdf65e0070fc30989788cda68d
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-06 21:16:13 +0200

    Added the Route of the AI Story Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import AIStoryVisualizedPage from './pages/AIStoryVisualizedPage/AIStoryVisualizedPage';
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/AIPage" element={<AIStoryVisualizedPage />} />
 
     </Routes>
   );
