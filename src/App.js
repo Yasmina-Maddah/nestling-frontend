6cb3975ad07b5e9f9c207f4270c08d84f83d5067
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-07 15:23:58 +0200

    Added the Route of the Profile Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import AIStoryVisualizedPage from './pages/AIStoryVisualizedPage/AIStoryVisualizedPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Profile" element={<ProfilePage />} />
       <Route path="/AIPage" element={<AIStoryVisualizedPage />} />
 
     </Routes>
