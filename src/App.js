5fa2f2d7b27c6b5e999eb26b2e550a0c940d7835
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-07 20:37:48 +0200

    Edited the Routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
-import AIStoryVisualizedPage from './pages/AIStoryVisualizedPage/AIStoryVisualizedPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import AIStoryVisualizedPage from './pages/AIStoryVisualizedPage/AIStoryVisualizedPage';
 
 
 const App = () => {
