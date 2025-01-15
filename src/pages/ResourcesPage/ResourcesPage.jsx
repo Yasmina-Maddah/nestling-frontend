909057acb465f5e5c700a3fc5b93adfb7315068d
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 13:48:39 +0200

    Edited the whole resources page

--- src/pages/ResourcesPage/ResourcesPage.jsx
+++ src/pages/ResourcesPage/ResourcesPage.jsx
@@ -1,44 +1,33 @@
 import React from "react";
-import Sidebar from "../../components/Sidebar/Sidebar"; 
-import Article1 from "../../assets/images/Article1.png";
-import Article2 from "../../assets/images/Article2.png";
-import Article3 from "../../assets/images/Article3.png";
-import Article4 from "../../assets/images/Article4.png";
-import Article5 from "../../assets/images/Article5.png";
-import Article6 from "../../assets/images/Article6.png";
-import "./ResourcesPage.css";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import styles from "./ResourcesPage.css";
 
+const articles = [
+    { title: "Early Childhood", image: "path_to_image/Article1.png" },
+    { title: "Psychology", image: "path_to_image/Article2.png" },
+    { title: "Communication", image: "path_to_image/Article3.png" },
+];
 
 const ResourcesPage = () => {
-  const articles = [
-    { title: "Early Childhood", image: Article1 },
-    { title: "Phycology", image: Article2 },
-    { title: "Communication", image: Article3 },
-    { title: "Ways of Learning", image: Article4 },
-    { title: "Raise a Reader", image: Article5 },
-    { title: "Importance of Crafting", image: Article6},
-  ];
-
-  return (
-    <div className="resources-page">
-      <Sidebar />
-      <main className="main-content">
-        <h1>Explore and Grow: Articles to Boost Your Child's Skills</h1>
-        
-        <section className="articles-section">
-          <h2>Articles</h2>
-          <div className="articles-grid">
-            {articles.map((article, index) => (
-              <div className="article-card" key={index}>
-                <img src={article.image} alt={article.title} />
-                <p>{article.title}</p>
-              </div>
-            ))}
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+    return (
+        <div className={styles.resourcesPage}>
+            <Sidebar />
+            <main className={styles.mainContent}>
+                <h1>Explore and Grow</h1>
+                <section className={styles.articlesSection}>
+                    <h2>Articles</h2>
+                    <div className={styles.articlesGrid}>
+                        {articles.map((article, index) => (
+                            <div className={styles.articleCard} key={index}>
+                                <img src={article.image} alt={article.title} />
+                                <p>{article.title}</p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 };
 
 export default ResourcesPage;
