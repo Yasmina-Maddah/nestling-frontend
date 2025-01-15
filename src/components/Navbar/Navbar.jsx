d9b8e5033d18155cd70f64c0614f919011bdcf2b
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 14:42:58 +0200

    Edited the Navbar dor scrolling purpose

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import './Navbar.css';
 import Logo from '../../assets/logo/1.png';
 
-
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={Logo} alt="Nestling Logo" />
-      </div>
-      <ul className="navbar-links">
-        <li><a href="#features">Features</a></li>
-        <li><a href="#how-it-works">How It Works</a></li>
-        <li><a href="#resources">Resources</a></li>
-        <li><a href="#contact">About Us</a></li>
-      </ul>
-      <div className="navbar-buttons">
-        <button className="navbar-button login-button">Login</button>
-        <button className="navbar-button signup-button">Sign Up</button>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src={Logo} alt="Nestling Logo" />
+            </div>
+            <ul className="navbar-links">
+                <li><a href="#features">Features</a></li>
+                <li><a href="#how-it-works">How It Works</a></li>
+                <li><a href="#resources">Resources</a></li>
+                <li><a href="#about-us">About Us</a></li>
+            </ul>
+            <div className="navbar-buttons">
+                <button className="navbar-button login-button">Login</button>
+                <button className="navbar-button signup-button">Sign Up</button>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
