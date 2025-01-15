98f65d21ea55bd26cc8da2c6c28ef3c8b2b4eb36
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 15:01:26 +0200

    Navigated from Landing Pagee to Login ad SignUp Pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import Logo from '../../assets/logo/1.png';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
+    const handleSignUpClick = () => {
+        navigate('/signup');
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
@@ -15,8 +26,12 @@ const Navbar = () => {
                 <li><a href="#about-us">About Us</a></li>
             </ul>
             <div className="navbar-buttons">
-                <button className="navbar-button login-button">Login</button>
-                <button className="navbar-button signup-button">Sign Up</button>
+                <button className="navbar-button login-button" onClick={handleLoginClick}>
+                    Login
+                </button>
+                <button className="navbar-button signup-button" onClick={handleSignUpClick}>
+                    Sign Up
+                </button>
             </div>
         </nav>
     );
