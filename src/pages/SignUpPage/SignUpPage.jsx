02809e7b7f931642693a218b2d3e0eac181e4ea8
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 15:28:17 +0200

    Added classes to Auth.css

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import '../LoginPage/Auth.css';
+import '../LoginPage/LoginPage.css';
+import './SignUpPage.css'
 import Logo from '../../assets/logo/2.png';
 
 const SignUp = () => {
     return (
-        <div className="auth-page">
-            <div className="auth-container">
-                <img src={Logo} alt="Nestling Logo" className="auth-logo" />
-                <h1>Nestling</h1>
-                <p>"Unlocking Potential, One Story at a Time"</p>
-                <form>
-                    <input type="email" placeholder="Email" required className="auth-input-field" />
-                    <input type="text" placeholder="Username" required className="auth-input-field" />
-                    <input type="password" placeholder="Password" required className="auth-input-field" />
-                    <input type="password" placeholder="Confirm Password" required className="auth-input-field" />
-                    <button type="submit" className="auth-submit-button">Sign Up</button>
-                </form>
-            </div>
+        <div className="container">
+            <img src={Logo} alt="Nestling Logo" className="logo" />
+            <h1>Nestling</h1>
+            <p>"Unlocking Potential, One Story at a Time"</p>
+            <form>
+                <input type="email" placeholder="Email" required className="input-field" />
+                <input type="text" placeholder="Username" required className="input-field" />
+                <input type="password" placeholder="Password" required className="input-field" />
+                <input type="password" placeholder="Confirm Password" required className="input-field" />
+                <button type="submit" className="submit-button-one">Sign Up</button>
+            </form>
         </div>
     );
 };
