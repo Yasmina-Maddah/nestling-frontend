906b28cd0143f31a573712c30db0f2889ed7e720
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 13:31:56 +0200

    Edited the App.js, to add the Routes of the Login and SignUp pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import AIStoryVisualizedPage from './pages/AIStoryVisualizedPage/AIStoryVisualizedPage';
@@ -10,6 +12,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path ="/SignUp" element={<SignUpPage />} />
+      <Route path="/Login" element={<LoginPage />} />
       <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/Profile" element={<ProfilePage />} />
       <Route path="/AIPage" element={<AIStoryVisualizedPage />} />
