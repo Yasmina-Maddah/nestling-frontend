9e7b370d7047e2697e79bbd5840852061752cdaa
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-19 00:50:02 +0200

    Added a class to activate the link when hover

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import Logo from '../../assets/logo/1.png';
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [activeLink, setActiveLink] = useState(''); // State to track the active link
 
     const handleLoginClick = () => {
         navigate('/login');
@@ -14,16 +15,52 @@ const Navbar = () => {
         navigate('/signup');
     };
 
+    const handleLinkClick = (link) => {
+        setActiveLink(link); // Update the active link
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
                 <img src={Logo} alt="Nestling Logo" />
             </div>
             <ul className="navbar-links">
-                <li><a href="#features">Features</a></li>
-                <li><a href="#how-it-works">How It Works</a></li>
-                <li><a href="#resources">Resources</a></li>
-                <li><a href="#about-us">About Us</a></li>
+                <li>
+                    <a
+                        href="#features"
+                        className={activeLink === 'features' ? 'active' : ''}
+                        onClick={() => handleLinkClick('features')}
+                    >
+                        Features
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#how-it-works"
+                        className={activeLink === 'how-it-works' ? 'active' : ''}
+                        onClick={() => handleLinkClick('how-it-works')}
+                    >
+                        How It Works
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#resources"
+                        className={activeLink === 'resources' ? 'active' : ''}
+                        onClick={() => handleLinkClick('resources')}
+                    >
+                        Resources
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#about-us"
+                        className={activeLink === 'about-us' ? 'active' : ''}
+                        onClick={() => handleLinkClick('about-us')}
+                    >
+                        About Us
+                    </a>
+                </li>
             </ul>
             <div className="navbar-buttons">
                 <button className="navbar-button login-button" onClick={handleLoginClick}>
